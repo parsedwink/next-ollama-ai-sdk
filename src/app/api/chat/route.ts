132224bcc1c19eba889e1737ed62f1be180f6c8d
Leo Nistor <leo.nistor@gmail.com>
2025-08-16 11:40:03 +0300

    ai sdk example

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,19 @@
+// import { openai } from "@ai-sdk/openai"
+import { ollama } from "ollama-ai-provider-v2"
+import { streamText, UIMessage, convertToModelMessages } from "ai"
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30
+
+const model = ollama("hf.co/QuantFactory/EuroLLM-9B-Instruct-GGUF:Q4_K_M")
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json()
+
+  const result = streamText({
+    model,
+    messages: convertToModelMessages(messages),
+  })
+
+  return result.toUIMessageStreamResponse()
+}
