0faca675b2f57fb0f9a08c1538f6ca36515b54ae
Leo Nistor <leo.nistor@gmail.com>
2025-08-16 14:55:43 +0300

    jsoning db REST routes

--- src/app/api/db/route.ts
+++ src/app/api/db/route.ts
@@ -3,13 +3,30 @@ import { Jsoning } from "jsoning"
 
 const db = new Jsoning("db/testdb.json")
 
-db.clear()
-db.set("one", "unu")
-
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams
-  const key = searchParams.get("key") ?? "undefined"
-  const val = (await db.get(key)) ?? "undefined"
+  const key = searchParams.get("key")
 
-  return NextResponse.json({ key, val })
+  if (key) {
+    const val = (await db.get(key)) ?? "UNDEFINED"
+    return NextResponse.json({ key, val })
+  } else {
+    const all = await db.all()
+    return NextResponse.json({ all })
+  }
+}
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+  const { key, val } = body
+
+  await db.set(key, val)
+  return NextResponse.json({ result: "ok" }, { status: 201 })
+}
+
+export async function DELETE(req: NextRequest) {
+  const body = await req.json()
+  const { key } = body
+
+  await db.delete(key)
+  return NextResponse.json({ result: "ok" }, { status: 202 })
 }
