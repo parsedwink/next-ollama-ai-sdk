1f89f8e5c667bac2d03234e992d49fd8a47538ec
Leo Nistor <leo.nistor@gmail.com>
2025-08-17 12:00:55 +0300

    fix translate flex

--- src/app/sections/translate.tsx
+++ src/app/sections/translate.tsx
@@ -1,41 +1,54 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Textarea } from "@/components/ui/textarea"
 import { useChat } from "@ai-sdk/react"
 import { useState } from "react"
 
 export default function Translate() {
   const [input, setInput] = useState("")
   const { messages, sendMessage } = useChat()
-  return (
-    <div className="flex flex-col w-full">
-      {messages.map((message) => (
-        <div key={message.id} className="whitespace-pre-wrap">
-          {message.role === "user" ? "User: " : "AI: "}
-          {message.parts.map((part, i) => {
-            switch (part.type) {
-              case "text":
-                return <div key={`${message.id}-${i}`}>{part.text}</div>
-            }
-          })}
-        </div>
-      ))}
 
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          sendMessage({ text: input })
-          setInput("")
-        }}
-      >
-        <textarea
-          className="border border-accent rounded"
+  return (
+    <form
+      className="h-full"
+      onSubmit={(e) => {
+        e.preventDefault()
+        sendMessage({ text: input })
+        setInput("")
+      }}
+    >
+      <div className="flex flex-col w-full h-full gap-2">
+        <Textarea
+          id="result"
+          className="w-full rounded-md border p-4 grow"
+          placeholder=""
+        >
+          {messages
+            .filter((m) => m.role === "assistant")
+            .map((m) => (
+              <div key={m.id} className="whitespace-pre-wrap">
+                {m.parts
+                  .filter((p) => p.type === "text")
+                  .map((part, i) => (
+                    <div key={`${m.id}-${i}`}>{part.text}</div>
+                  ))}
+              </div>
+            ))}
+        </Textarea>
+
+        <Textarea
+          id="source"
+          className="border border-accent rounded grow"
           value={input}
           placeholder="Say something..."
           onChange={(e) => setInput(e.currentTarget.value)}
         />
-        <Button type="submit">Send</Button>
-      </form>
-    </div>
+        <div className="grow-0">
+          <Button type="submit">Send</Button>
+        </div>
+      </div>
+    </form>
   )
 }
