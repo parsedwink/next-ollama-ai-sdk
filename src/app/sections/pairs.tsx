8c15f8b6ed703545de98207b33b6f1dc8bb93472
Leo Nistor <leo.nistor@gmail.com>
2025-08-17 07:45:58 +0300

    useSWRMutation start in pairs

--- src/app/sections/pairs.tsx
+++ src/app/sections/pairs.tsx
@@ -1,9 +1,20 @@
 "use client"
 
 import useSWR, { SWRConfig } from "swr"
+import useSWRMutation from "swr/mutation"
 
 const fetcher = (url: string | URL | Request) =>
   fetch(url).then((r) => r.json())
+
+async function sendRequest(
+  url: string | URL | Request,
+  { arg }: { arg: { username: string } },
+) {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(arg),
+  }).then((res) => res.json())
+}
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -19,12 +30,17 @@ import {
 import { Suspense } from "react"
 
 export default function Pairs() {
-  // const { data = { all: [] }, error } = useSWR("/api/db", fetcher, {
   const { data = { all: [] }, error } = useSWR("/api/db", fetcher, {
     suspense: true,
     fallbackData: { all: { " ": " " } },
   })
   const all = data.all as Record<string, string>
+
+  const { trigger, isMutating } = useSWRMutation("/api/db", sendRequest)
+
+  if (error) {
+    return <div className="bg-destructive">{`Error: ${error}`}</div>
+  }
 
   return (
     <SWRConfig>
@@ -46,7 +62,20 @@ export default function Pairs() {
                     <Input name="val" placeholder="romana" />
                   </TableHead>
                   <TableHead>
-                    <Button variant="default" size="icon" className="size-8">
+                    <Button
+                      variant="default"
+                      size="icon"
+                      className="size-8"
+                      disabled={isMutating}
+                      onClick={async () => {
+                        try {
+                          console.log("try")
+                          // const result = await trigger({ username: "johndoe" })
+                        } catch (e) {
+                          // error handling
+                        }
+                      }}
+                    >
                       <CirclePlusIcon />
                     </Button>
                   </TableHead>
