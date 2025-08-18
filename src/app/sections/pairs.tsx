159b235dc193f76e04d258d19b512dd9c0a0e78d
Leo Nistor <leo.nistor@gmail.com>
2025-08-18 20:42:04 +0300

    pairs mmmh

--- src/app/sections/pairs.tsx
+++ src/app/sections/pairs.tsx
@@ -1,61 +1,20 @@
 "use client"
 
-import useSWR, { SWRConfig } from "swr"
-import useSWRMutation from "swr/mutation"
-
-const fetcher = (url: string | URL | Request) =>
-  fetch(url).then((r) => r.json())
-
-async function sendPost(
-  url: string | URL | Request,
-  { arg }: { arg: { key: string; val: string } },
-) {
-  return fetch(url, {
-    method: "POST",
-    body: JSON.stringify(arg),
-  }).then((res) => res.json())
-}
-
-async function sendDelete(
-  url: string | URL | Request,
-  { arg }: { arg: { key: string } },
-) {
-  return fetch(url, {
-    method: "DELETE",
-    body: JSON.stringify(arg),
-  }).then((res) => res.json())
-}
+import { useEffect, useState } from "react"
 
 import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
-import { Suspense, useEffect, useState } from "react"
-
-import { Pill, PillButton, PillStatus } from "@/components/ui/kibo-ui/pill"
-import { XIcon } from "lucide-react"
-
 import { ControlGroup, ControlGroupItem } from "@/components/ui/control-group"
 import {
   InputBase,
   InputBaseControl,
   InputBaseInput,
 } from "@/components/ui/input-base"
-
-// ---
+import { Pill, PillButton, PillStatus } from "@/components/ui/kibo-ui/pill"
+import { PlusIcon, XIcon } from "lucide-react"
 
 export default function Pairs() {
-  const { data = { all: [] }, error } = useSWR("/api/db", fetcher, {
-    suspense: true,
-    fallbackData: { all: { " ": " " } },
-  })
-  const all = data.all as Record<string, string>
-
-  const { trigger: triggerPost, isMutating } = useSWRMutation(
-    "/api/db",
-    sendPost,
-  )
-
   //--- state
-  const { trigger: triggerDelete } = useSWRMutation("/api/db", sendDelete)
+  const [pairs, setPairs] = useState<Record<string, string>>({})
   const [input_key, setInput_key] = useState("")
   const [input_val, setInput_val] = useState("")
   const [addOk, setAddOk] = useState(false)
@@ -66,8 +25,14 @@ export default function Pairs() {
   }, [input_key, input_val])
 
   //--- handlers
-  function handleDel(e: unknown, key: string) {
-    triggerDelete({ key })
+  async function handleDel(e: unknown, key: string) {
+    const result = await fetch("/api/db", {
+      method: "DELETE",
+      body: JSON.stringify({ key }),
+    })
+
+    if (result.status === 202) {
+    }
   }
 
   async function handleAdd() {
@@ -75,93 +40,92 @@ export default function Pairs() {
       return
     }
     try {
-      // const result = await trigger({
-      await triggerPost({
-        key: input_key,
-        val: input_key,
+      await fetch("/api/db", {
+        method: "POST",
+        body: JSON.stringify({
+          key: input_key,
+          val: input_val,
+        }),
       })
     } catch (error) {
       console.log(`${error}`)
     }
-
-    // reset
+    // TODO: fix
+    // setPairs(Object.defineProperty(pairs, input_key, input_val))
     setInput_key("")
     setInput_val("")
   }
 
-  // ---
-
-  if (error) {
-    return <div className="bg-destructive">{`Error: ${error}`}</div>
-  }
+  useEffect(() => {
+    fetch("/api/db")
+      .then((response) => response.json())
+      .then((data) => {
+        setPairs(data.all)
+      })
+  }, [])
 
   return (
-    <SWRConfig>
+    <div>
       <h3>Pairs</h3>
 
-      <Suspense fallback={<div>loading...</div>}>
-        <form id="form_add_pair">
-          <div className="flex flex-col gap-2 p-2">
-            {/* inputs group */}
-            <ControlGroup>
-              <ControlGroupItem>
-                <InputBase>
-                  <InputBaseControl>
-                    <InputBaseInput
-                      id="form_key"
-                      placeholder="sursa"
-                      value={input_key ?? ""}
-                      onChange={(e) => setInput_key(e.target.value)}
-                    />
-                  </InputBaseControl>
-                </InputBase>
-              </ControlGroupItem>
-              <ControlGroupItem>
-                <InputBase>
-                  <InputBaseControl>
-                    <InputBaseInput
-                      id="form_val"
-                      placeholder="traducere"
-                      value={input_val ?? ""}
-                      onChange={(e) => setInput_val(e.target.value)}
-                    />
-                  </InputBaseControl>
-                </InputBase>
-              </ControlGroupItem>
-              <ControlGroupItem>
-                <Button
-                  disabled={!addOk || isMutating}
-                  onClick={() => handleAdd()}
-                >
-                  <PlusIcon />
-                </Button>
-              </ControlGroupItem>
-            </ControlGroup>
+      <form id="form_add_pair">
+        <div className="flex flex-col gap-2 p-2">
+          {/* inputs group */}
+          <ControlGroup>
+            <ControlGroupItem>
+              <InputBase>
+                <InputBaseControl>
+                  <InputBaseInput
+                    id="form_key"
+                    placeholder="sursa"
+                    value={input_key ?? ""}
+                    onChange={(e) => setInput_key(e.target.value)}
+                  />
+                </InputBaseControl>
+              </InputBase>
+            </ControlGroupItem>
+            <ControlGroupItem>
+              <InputBase>
+                <InputBaseControl>
+                  <InputBaseInput
+                    id="form_val"
+                    placeholder="traducere"
+                    value={input_val ?? ""}
+                    onChange={(e) => setInput_val(e.target.value)}
+                  />
+                </InputBaseControl>
+              </InputBase>
+            </ControlGroupItem>
+            <ControlGroupItem>
+              <Button disabled={!addOk} onClick={() => handleAdd()}>
+                <PlusIcon />
+              </Button>
+            </ControlGroupItem>
+          </ControlGroup>
 
-            {/* w/ pills */}
+          {/* pills list */}
 
-            <div className="flex flex-wrap gap-1">
-              {Object.entries(all).map(([key, val]) => {
-                return (
-                  <Pill key={key}>
-                    <PillStatus>{key}</PillStatus>
-                    {val}
-                    <PillButton
-                      size="icon"
-                      variant="ghost"
-                      onClick={async (e) => handleDel(e, key)}
-                    >
-                      <XIcon className="text-neutral-500" size={12} />
-                    </PillButton>
-                  </Pill>
-                )
-              })}
-            </div>
-
-            {/* - */}
+          <div className="flex flex-wrap gap-1">
+            {Object.entries(pairs).map(([key, val]) => {
+              return (
+                <Pill key={key}>
+                  <PillStatus>{key}</PillStatus>
+                  {val}
+                  <PillButton
+                    size="icon"
+                    variant="ghost"
+                    onClick={async (e) => handleDel(e, key)}
+                  >
+                    <XIcon className="text-neutral-500" size={12} />
+                  </PillButton>
+                </Pill>
+              )
+            })}
           </div>
-        </form>
-      </Suspense>
-    </SWRConfig>
+
+          {/* - */}
+        </div>
+      </form>
+    </div>
   )
 }
